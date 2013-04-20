9a6a737f3a8f148926bfad7d3f88e6590608f5b8
krukas <maikel_martens_1@hotmail.com>
2013-04-20 10:39:55 +0200

    Create defines for __SITE_PATH, __APPLICATION_PATH and __CONFIG_PATH. Create common.php -Added function load_class -added function random_string -load the classes --page --template --database

---
+++ index.php
@@ -0,0 +1,16 @@
+<?PHP
+
+/* * * define the site path * * */
+$site_path = realpath(dirname(__FILE__)).'/';
+define('__SITE_PATH', $site_path);
+
+/* * * define the application path * * */
+$application_path = realpath(dirname(__FILE__)).'/application/';
+define('__APPLICATION_PATH', $application_path);
+
+/* * * define the config path * * */
+$config_path = realpath(dirname(__FILE__)).'/application/config/';
+define('__CONFIG_PATH', $config_path);
+
+/* * * include the common.php * * */
+include __APPLICATION_PATH . 'core/common.php';
