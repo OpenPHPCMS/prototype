8cdef8a145f195e666793b5af1d1dce539d3b58a
Gijs <jikizuari@gmail.com>
2013-04-24 12:36:14 +0200

    Settings, added icons, style fixes
    Changed settings, added several icons, style modified for table hover
    and button icons.

--- admin/views/settings.php
+++ admin/views/settings.php
@@ -1,7 +1,3 @@
-<?php
-	require_once('validation/validate_settings.php');	//Validation needed for this form
-	require_once('essentials/date_and_time.php');
-?>
 <h1>General settings</h1>
 
 <!-- Form -->
@@ -11,27 +7,27 @@
 		<hr/>
 			<p><label class="formlabel">Title</label> 		
 					<label class="help">Your website's name</label> 					
-						<input class="textfield" type="text" name="title" value="<?php echo $_GET['title'] ?>"/>			
+						<input class="textfield" type="text" name="title" value="<?php echo $title ?>"/>			
 			</p>
 			<p><label class="formlabel">Slogan</label> 		
 					<label class="help">Your website's slogan</label>					
-						<input class="textfield" type="text" name="slogan" value="<?php echo $_GET['slogan'] ?>"/>
+						<input class="textfield" type="text" name="slogan" value="<?php echo $slogan ?>"/>
 			</p>
 			<p><label class="formlabel">Description</label> 
 					<label class="help">Your website's description</label>				
-						<input class="textfield" type="text" name="description" value="<?php echo $_GET['description'] ?>"/>
+						<input class="textfield" type="text" name="description" value="<?php echo $description ?>"/>
 			</p>
 			<p><label class="formlabel">Titleformat</label> 
 					<label class="help">Ex. [title] - [page]</label>					
-						<input class="textfield" type="text" name="titleformat" value="<?php echo $_GET['titleformat'] ?>"/>
+						<input class="textfield" type="text" name="titleformat" value="<?php echo $titleformat ?>"/>
 			</p>
 			<p><label class="formlabel">E-mail</label> 		
 					<label class="help">Your e-mail address</label>						
-						<input class="textfield" type="text" name="email" value="<?php echo $_GET['email'] ?>"/>
+						<input class="textfield" type="text" name="email" value="<?php echo $email ?>"/>
 			</p>
 			<p><label class="formlabel">Base URL</label> 	
 					<label class="help">Ex. http://www.openphpcms.org/</label>			
-						<input class="textfield" type="text" name="baseurl" value="<?php echo $_GET['baseurl'] ?>"/>
+						<input class="textfield" type="text" name="baseurl" value="<?php echo $baseurl ?>"/>
 			</p>
 		<span class="formtitle">Date & Time</span>
 		<hr/>
@@ -39,7 +35,7 @@
 			<select name="gmt" id="gmt">
 				<?php 
 					//Declared in essentials/date_and_time.php
-					get_timezone_select() 
+					echo $timezone;
 				?>
 			</select>
 		</p>
@@ -47,7 +43,7 @@
 			<select name="dateFormatLong">
 				<?php
 					//Declared in essentials/date_and_time.php
-					generateLongDateFormat()
+					echo $longDateFormat;
 				?>
 			</select>
 		</p>
@@ -55,7 +51,7 @@
 			<select name="dateFormatLong">
 				<?php
 					//Declared in essentials/date_and_time.php
-					generateMediumDateFormat()
+					echo $mediumDateFormat;
 				?>
 			</select>
 		</p>
@@ -63,7 +59,7 @@
 			<select name="dateFormatLong">
 				<?php
 					//Declared in essentials/date_and_time.php
-					generateShortDateFormat()
+					echo $shortDateFormat;
 				?>
 			</select>
 		</p>
