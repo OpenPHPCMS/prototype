94437b104f2fabfcc8bfe68ffc81462d2be0b4df
krukas <maikel_martens_1@hotmail.com>
2013-04-25 00:18:52 +0200

    get general setting from OPC_Settings changed openphpcms.sql

--- admin/settings.php
+++ admin/settings.php
@@ -14,12 +14,12 @@ load_class('DateAndTime', 'lib');
 $date_and_time = new DateAndTime();
 $timezone_arr = $date_and_time->getTimezoneArray();
 
-$data['title'] = '';
-$data['slogan'] = '';
-$data['description'] = '';
-$data['titleformat'] = '';
-$data['email'] = '';
-$data['baseurl'] = '';
+$data['title'] 			= OPC_Settings::get('title');
+$data['slogan'] 		= OPC_Settings::get('slogan');
+$data['description'] 	= OPC_Settings::get('description');
+$data['titleformat'] 	= OPC_Settings::get('title_format');
+$data['email'] 			= OPC_Settings::get('admin_email');
+$data['baseurl'] 		= OPC_Settings::get('base_url');
 
 $data['timezone'] = '';
 foreach ( $timezone_arr as $key=>$value ) {
--- openphpcms.sql
+++ openphpcms.sql
@@ -1,3 +1,27 @@
+-- --------------------------------------------------------
+--
+-- Table structure for table `OPC_Pages`
+--
+DROP TABLE IF EXISTS `OPC_Pages`;
+CREATE TABLE `OPC_Pages` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  `title` varchar(128) NOT NULL,
+  `type` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+-- --------------------------------------------------------
+--
+-- Table structure for table `OPC_Page_content`
+--
+DROP TABLE IF EXISTS `OPC_Page_content`;
+CREATE TABLE `OPC_Page_content` (
+  `page_id` int(10) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `value` text NOT NULL,
+  PRIMARY KEY (`page_id`,`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+-- --------------------------------------------------------
 --
 -- Table structure for table `OPC_Sessions`
 --
@@ -8,7 +32,7 @@ CREATE TABLE `OPC_Sessions` (
   `data` text NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
+-- --------------------------------------------------------
 --
 -- Table structure for table `OPC_Settings`
 --
@@ -20,6 +44,18 @@ CREATE TABLE `OPC_Settings` (
   PRIMARY KEY (`setting_name`,`appid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Ddata for table `OPC_Settings`
+--
+INSERT INTO `OPC_Settings` (`appid`, `setting_name`, `setting_value`) VALUES
+('core', 'base_url', 'http://openphpcms,org'),
+('core', 'slogan', ''),
+('core', 'title', 'Open PHP CMS'),
+('core', 'description', ''),
+('core', 'title_format', '[title]-[page]'),
+('core', 'admin_email', '');
+
+-- --------------------------------------------------------
 --
 -- Table structure for table `OPC_Users`
 --
@@ -34,3 +70,11 @@ CREATE TABLE `OPC_Users` (
   `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`username`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Data for table `OPC_Users`
+--
+INSERT INTO `OPC_Users` (`username`, `password`, `level`, `name`, `surname`, `email`, `create_date`) VALUES
+('Admin', '861ef23e2b79e42d8c2a8dde30add3ab8632b2bdacc6ca42f275d97ad231935f449315581fea5a4f70b35ce6a533f752b03be2174ff4386da0f009ce66a9d16df0084ff7', 3, 'Admin', 'Admin', 'Admin', '2013-04-22 18:47:11'),
+('User', '861ef23e2b79e42d8c2a8dde30add3ab8632b2bdacc6ca42f275d97ad231935f449315581fea5a4f70b35ce6a533f752b03be2174ff4386da0f009ce66a9d16df0084ff7', 1, 'user', 'user', 'user', '2013-04-22 22:40:09'),
+('Dev', '861ef23e2b79e42d8c2a8dde30add3ab8632b2bdacc6ca42f275d97ad231935f449315581fea5a4f70b35ce6a533f752b03be2174ff4386da0f009ce66a9d16df0084ff7', 2, 'dev', 'dev', 'dev', '2013-04-22 22:40:09'),
