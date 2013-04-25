156e6a6a84b96d9a81cdea178ae9ba71052f3ebb
krukas <maikel_martens_1@hotmail.com>
2013-04-25 14:47:29 +0200

    added front end demo message

--- index.php
+++ index.php
@@ -17,3 +17,41 @@ require(__APPLICATION_PATH.'core/Errors.php');
 
 /* * * include the System Initialization File * * */
 require(__APPLICATION_PATH . 'core/OpenPhpCms.php');
+
+
+
+
+//Temp Demo
+?>
+<html>
+<head>
+	<title>OpenPhpCms Daily Build Prototype</title>
+</head>
+<body>
+
+<h3>This is an prototype and there is no front end yet</h3>
+<p>
+	You can see the admin panel <a href="<?PHP echo base_url('admin') ?> ">here</a>
+</p>
+<p>Admin panel accounts: </p>
+<table>
+  <tr>
+  	<th>Username</th>
+  	<th>Password</th>
+  </tr>
+<tr>
+	<td>admin</td>
+	<td>admin</td>
+</tr>
+<tr>
+	<td>dev</td>
+	<td>dev</td>
+</tr>
+<tr>
+	<td>user</td>
+	<td>user</td>
+</tr>
+</table>
+
+</body>
+</html>
