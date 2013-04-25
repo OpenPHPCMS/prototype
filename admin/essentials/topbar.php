e814ae3deb04d5de7a4066647103ab58f92b70b9
krukas <maikel_martens_1@hotmail.com>
2013-04-25 14:35:22 +0200

    Fixed dead dubble url

--- admin/essentials/topbar.php
+++ admin/essentials/topbar.php
@@ -1,5 +1,5 @@
 <section class="topbar">
 	<?php if(!empty($_SESSION['user_username'])): ?>
-	<div class="right">Welcome, <?PHP echo $_SESSION['user_name'] .' '. $_SESSION['user_surname'] ?> <a class="buttonsmall bluebut" href='"<?php echo base_url('admin/logout.php') ?>"'>Log out</a></div>
+	<div class="right">Welcome, <?PHP echo $_SESSION['user_name'] .' '. $_SESSION['user_surname'] ?> <a class="buttonsmall bluebut" href="<?php echo base_url('admin/logout.php') ?>">Log out</a></div>
 	<?php endif; ?>
 </section>
